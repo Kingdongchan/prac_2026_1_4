904763d80e723400ecc2aec18442e19ab6762209
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-04 17:20:38 +0900

    제목완성

--- plus_button.py
+++ plus_button.py
@@ -3,11 +3,20 @@ from tkinter import messagebox
 
 import object.button as bt
 import object.frame as frm
-import object.label as leb
+import object.label as lab
 import object.text as tx
 
 root = tk.Tk()
-
+root.geometry("600x400")
+#상위(제목, txt공간) 프레임
+top_frm = frm.frame(root, 400, 50, "top", "white")
+top_frm_obj = top_frm.frm_maker()
+#'제목' 라벨 생성
+title_lb = lab.label(top_frm_obj, "제목: ", "left")
+title_lb.lb_maker()
+#제목 옆에 텍스트 파일
+title_txt = tx.text(top_frm_obj, 60, 1, "left")
+title_txt.txt_maker()
 
 
 root.mainloop()
