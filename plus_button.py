247c5d4614449f1ff09aea5ac4ac9c15cd99cf71
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-04 17:07:38 +0900

    +버튼 로직 준비

---
+++ plus_button.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+
+import object.button as bt
+import object.frame as frm
+import object.label as leb
+import object.text as tx
+
+root = tk.Tk()
+
+
+
+root.mainloop()
