3381fe69b21867d1c1ecb7f37dc1b56932d1411d
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-04 17:33:08 +0900

    내용와 텍스트 생성 완료

--- plus_button.py
+++ plus_button.py
@@ -7,7 +7,7 @@ import object.label as lab
 import object.text as tx
 
 root = tk.Tk()
-root.geometry("600x400")
+root.geometry("400x600")
 #상위(제목, txt공간) 프레임
 top_frm = frm.frame(root, 400, 50, "top", "white")
 top_frm_obj = top_frm.frm_maker()
@@ -15,8 +15,16 @@ top_frm_obj = top_frm.frm_maker()
 title_lb = lab.label(top_frm_obj, "제목: ", "left")
 title_lb.lb_maker()
 #제목 옆에 텍스트 파일
-title_txt = tx.text(top_frm_obj, 60, 1, "left")
+title_txt = tx.text(top_frm_obj, 50, 1, "left")
 title_txt.txt_maker()
+#하위(제목, txt공간0 프레임
+under_frm = frm.frame(root, 400, 250, "top", "white")
+under_frm_obj = under_frm.frm_maker()
+#'내용' 라벨 생성
+cont_lb = lab.label(under_frm_obj, "내용: ", "left")
+cont_lb.lb_maker()
 
+cont_txt = tx.text(under_frm_obj, 50, 100, "left")
+cont_txt.txt_maker()
 
 root.mainloop()
