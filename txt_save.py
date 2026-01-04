f75b93db90c8a4b5fe1160b76b274d9d665044d3
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-04 18:13:25 +0900

    메인 프레인초기화 로직 생성

--- txt_save.py
+++ txt_save.py
@@ -1,13 +1,25 @@
 import tkinter as tk
-
+from tkinter import messagebox
 #세이브하는 로직을 만듬
 #저장 버튼을 누르면 배열에 들어가야 하고 
 #메인 틀 초기화후 내가 썻던 제목만 들어있는 버튼을 생성 
 #버튼을 누르면 내가 전에 썻던 내용을 볼 수 있겠끔 만드는 로직이 필요
 
-def file_save():
+#담을 공간 생성
+blank = []
+
+def file_save(title_txt, cont_txt, main_frm_obj):
     
-    t = title_txt.get("1.0","end-1c")
-    c = cont_txt.get("1.0","end-1c")
+    #제목와 내용에 쓰여진 것들 가져오기
+    t = title_txt.blank.get("1.0","end-1c")
+    c = cont_txt.blank.get("1.0","end-1c")
+
+    txt_sum = {"제목": t, "내용": c}
+
+    blank.append(txt_sum)
     
-     
+    messagebox.showinfo("저장", "저장이 완료되었습니다.")
+    #저장 후 메인 틀 초기화
+    for widget in main_frm_obj.winfo_children():
+        widget.destory()
+        
